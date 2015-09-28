88524c2756908dbcb0bec1658171fea0c5431078
Roberto Guerra <uris77@gmail.com>
2015-09-28 15:33:37 -0400

    Skeleton prod with db connection

--- src/pasmo_outreach/server/routes/core.clj
+++ src/pasmo_outreach/server/routes/core.clj
@@ -1,8 +1,9 @@
 (ns pasmo-outreach.server.routes.core
-  (:require [compojure.core :refer [defroutes GET]]))
+  (:require [compojure.core :refer [defroutes GET]]
+            [pasmo-outreach.server.db.users :as user-db]))
 
 (def json-resp {:headers {"Content-Type" "application/json"}})
 
 (defroutes sample-routes
-  (GET "/sample" req (assoc json-resp :body [1 2 3 4])))
+  (GET "/sample" req (assoc json-resp :body (user-db/all))))
 
