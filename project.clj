d1d8925dcfb8c4bdec0a02a7089b50aa042792e5
Roberto Guerra <uris77@gmail.com>
2015-09-28 20:45:31 -0400

    Updates project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pasmo-outreach "1.0.0-SNAPSHOT"
   :description "Basic CRUD for gathering information on outreach efforts."
-  :url "http://example.com/FIXME"
+  :url "http://outreach.pasmo.bz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
